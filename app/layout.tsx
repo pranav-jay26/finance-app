1a365fe5b06e8f4fcab576a469f59e8e2b0b8952
pranav-jayakumar <pranav.jayakumar@orchestro.ai>
2024-09-30 23:06:13 -0700

    add logo and styling to login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
+
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from 'next/font/google';
 import "./globals.css";
 import { 
   ClerkProvider,
@@ -9,16 +10,10 @@ import {
   SignInButton
 } from "@clerk/nextjs";
 
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter = Inter({
+  subsets: ['latin'],
+  weights: ['100', '400', '700', '900'],
+  variable: '--font-inter',
 });
 
 export const metadata: Metadata = {
@@ -34,9 +29,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className={`${inter.variable} antialiased`}>
           <SignedOut>
             <SignInButton />
           </SignedOut>
@@ -47,6 +40,6 @@ export default function RootLayout({
         </body>
       </html>
     </ClerkProvider>
-
   );
 }
+
