d2105cb8e78b30ac0181ada60db30b81ee941916
pranav-jayakumar <pranav.jayakumar@orchestro.ai>
2024-09-30 20:05:37 -0700

    Create initial project structure and base authentication system

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+      This is an authenticated route. You are able to see this because you are signed in.
+    </div>
+  )
+}
